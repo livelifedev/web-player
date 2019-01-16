d64936daf07e127fbb62585cb0b2b0d50db35e52
livelifedev <johnrubio93@gmail.com>
2019-01-17 09:09:07 +1100

    Continued working on browse page code, fixed JS errors

--- assets/js/songDB.js
+++ assets/js/songDB.js
@@ -2,5 +2,7 @@ let songDB = [
     {title: "Sunny Sky", artist: "radBad", album: "Roadblock", artwork: "album1.jpg", src: "test_audio1.mp3"}, 
     {title: "Rainbow Redemption", artist: "Sykideric", album: "Dereck", artwork: "album2.jpg", src: "test_audio2.mp3"},
     {title: "Alpha Mantis", artist: "Eating Clouds", album: "Papayas", artwork: "album6.jpg", src: "test_audio3.mp3"},
-    {title: "One Leg Theodore", artist: "Eating Clouds", album: "Papayas", artwork: "album6.jpg", src: "test_audio3.mp3"}  
+    {title: "One Leg Theodore", artist: "Eating Clouds", album: "Papayas", artwork: "album6.jpg", src: "test_audio3.mp3"},
+    {title: "Reckon Fate", artist: "Jumping Jonas", album: "Junkus", artwork: "album9.jpg", src: "test_audio3.mp3"}, 
+    {title: "Sandman Haunting", artist: "Purple Vitalia", album: "Opus", artwork: "album12.jpg", src: "test_audio3.mp3"} 
 ];
