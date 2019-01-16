c5a8f57a594ef9b58eb76772117ca7c1e9dddea0
livelifedev <johnrubio93@gmail.com>
2019-01-16 20:34:42 +1100

    Added code for loading random album picks on browse page (still some errors to be fixed)

--- assets/js/songDB.js
+++ assets/js/songDB.js
@@ -1,5 +1,6 @@
 let songDB = [
-    {title: "Sunny Sky", artist: "radBad", album: "Roadblock", src: "test_audio1.mp3"}, 
-    {title: "Rainbow Redemption", artist: "Sykideric", album: "Dereck", src: "test_audio2.mp3"},
-    {title: "Alpha Mantis", artist: "Eating Clouds", album: "Papayas", src: "test_audio3.mp3"} 
+    {title: "Sunny Sky", artist: "radBad", album: "Roadblock", artwork: "album1.jpg", src: "test_audio1.mp3"}, 
+    {title: "Rainbow Redemption", artist: "Sykideric", album: "Dereck", artwork: "album2.jpg", src: "test_audio2.mp3"},
+    {title: "Alpha Mantis", artist: "Eating Clouds", album: "Papayas", artwork: "album6.jpg", src: "test_audio3.mp3"},
+    {title: "One Leg Theodore", artist: "Eating Clouds", album: "Papayas", artwork: "album6.jpg", src: "test_audio3.mp3"}  
 ];
